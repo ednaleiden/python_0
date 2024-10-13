015371c23d2639563d48a0becf4ca4a712411b9c
ednaleiden <ednadupont@hotmail.com>
2024-10-12 21:53:16 -0500

    learning pytho n

---
+++ clase_3.py
@@ -0,0 +1,74 @@
+print("hello word")
+
+#variables
+#nombre + tipo + informacion a almacenar
+nombre = "Edna"
+print(nombre)
+
+#palabras reservadas si se pinta de un color diferente no puede usarse
+
+
+#cadena o string
+
+name = "Edna"
+print(name)
+
+#consultar el tipo de dato
+print(type(name))
+
+
+caracter =  'Leiden'
+
+#evidencio salto de linea
+caracter_1 =  '''Edna
+Leiden
+
+Oliver'''
+
+print(caracter)
+print(caracter_1)
+#cadenas string en python
+
+
+#indexasion -saber las posiciones del dinal al principio negativo
+print(name[0])
+
+
+#multiplicar o repetir ciertas veces una cadena
+
+print(name  + " " + caracter)
+print(name *5 )
+
+#consultar la longitud
+
+print(len(name))
+
+
+"""
+.count() 
+.capitalize()
+.title() 
+.swapcase() 
+.replace(,) 
+.split() 
+.strip() 
+.lstrip() 
+.rstrip() 
+.find()
+.index() 
+eval()	#Este y el siguiente son super métodos
+exec()
+
+"""
+
+print(name.lower())
+print(name.upper())
+print(name.strip()) #elimina los espacios
+
+
+
+#flotante
+
+#entero
+
+#booleano
---
+++ clase_5.py
@@ -0,0 +1,36 @@
+x = 10
+y = 5.678
+z = 1.2e6 # poner siempre la notacion cientifica si es un numero grande o pequeño
+a= 1.2e-6
+print(x)
+print(type(x))
+
+print(y)
+print(type(y))
+
+print(z)
+print(type(z))
+
+print(a)
+print(type(a))
+
+# cuando utilizamos numeros muy grandes o demasiados pequeños se utiliza notacion cientifica
+
+
+#operadores numericos
+a = 10
+b = 3
+
+print("Suma: ",a +b)
+print("Resta: ",a - b)
+print("Multiplicacion: ",a * b)
+print("Portenciacion :  ",a ** b)
+print("Division: ",a / b)
+print("Division 2 veces parte entera: ",a // b)
+print("Modulo: " , a % b)
+
+#a = a +2 
+#a+=2
+#a-=2
+print(a)
+
