da01d142a12307d1896c17955581e9ff9c94052c
ednaleiden <ednadupont@hotmail.com>
2024-10-12 22:25:25 -0500

    tuplas

---
+++ clase_10.py
@@ -0,0 +1,16 @@
+
+#listas de listas 
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(matrix)
+
+print(matrix[0])
+#tuplas
+
+numbers = (1,2,3,4,5)
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+#tupla no es un objeto , no son mutables es decir son inmutables
+numbers[0] = "unos"
+print(numbers)
