911dc80b9397c918d1ba9fb372741aa61c969e28
ednaleiden <ednadupont@hotmail.com>
2024-10-12 22:18:56 -0500

    metodo slice

---
+++ clase_9.py
@@ -0,0 +1,25 @@
+#metodo slice
+
+a = [1,2,3,4,5]
+
+b = a
+print(a) 
+print(b)
+
+del a [0]
+print(id(a)) 
+print(id(b))
+
+c = a[:]
+print(id(a)) 
+print(id(b))
+print(id(c))
+
+a.append(6)
+print(id(a)) 
+print(id(b))
+print(id(c))
+
+print(a) 
+print(b)
+print(c)
